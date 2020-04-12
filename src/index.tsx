a90c82fb5066b07a936c9b2e076f32fe36d914fa
Sarah <sarahpads@gmail.com>
2020-04-12 20:59:27 -0300

    flexin: removed unneeded imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloProvider } from 'react-apollo';
-import ApolloClient from 'apollo-boost';
 
 import './index.css';
 import App from './App';
