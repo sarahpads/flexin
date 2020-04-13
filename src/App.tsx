feeac0f0e3121f39334c39df9161adfabae25311
Sarah <sarahpads@gmail.com>
2020-04-13 00:12:36 -0300

    flexin: progress point; updating apollo dependencies and migrating away from apollo-boost for subscription support

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react';
-import ApolloClient, { gql } from 'apollo-boost';
-import { ApolloProvider, Query } from 'react-apollo';
+import { WebSocketLink } from "@apollo/link-ws";
+import { ApolloClient, InMemoryCache, HttpLink, gql, ApolloProvider, split } from "@apollo/client"
+import { getMainDefinition } from '@apollo/client/utilities';
+import { SubscriptionClient } from "subscriptions-transport-ws";
 
 import { AuthContext } from './components/AuthProvider';
 
-const GET_USERS = gql`
+/*const GET_USERS = gql`
   {
     users {
       id,
@@ -13,10 +15,55 @@ const GET_USERS = gql`
   }
 `
 
+const NEW_CHALLENGE = gql`
+  subscription {
+    challengeCreated {
+      reps,
+      exercise { title },
+      user { name }
+    }
+  }
+`*/
+
 function App() {
   const auth = useContext(AuthContext)
 
+
+  const httpLink = new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+    headers: {
+      Authorization: `Bearer ${auth.getIdToken()}`,
+    }
+  });
+
+  const wsClient = new SubscriptionClient("ws://localhost:4000/subscriptions", {
+    reconnect: true
+  });
+
+  const wsLink = new WebSocketLink(wsClient);
+
+  const cache = new InMemoryCache();
+
+  // https://www.apollographql.com/docs/react/v3.0-beta/data/subscriptions/
+  const link = split(
+    // split based on operation type
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === 'OperationDefinition' &&
+        definition.operation === 'subscription'
+      );
+    },
+    wsLink,
+    httpLink,
+  );
+
   const client = new ApolloClient({
+    link,
+    cache
+  });
+
+  /*const client = new ApolloClient({
     uri: 'http://localhost:4000/graphql',
     request: (operation) => {
       operation.setContext((context: any) => ({
@@ -26,7 +73,7 @@ function App() {
         },
       }));
     }
-  });
+  });*/
 
   if (window.location.href.includes('consume')) {
     console.log(auth.consume());
@@ -39,21 +86,6 @@ function App() {
       {user.sub}
       <button onClick={auth.login}>Login</button>
 
-      <Query query={GET_USERS}>
-        {({ loading, error, data }: any) => {
-          if (!data) {
-            return <span>Loading or something</span>
-          }
-
-          return (
-            <div>
-              {data.users.map((user: any) => {
-                return <span key={user.id}>{user.email}</span>
-              })}
-            </div>
-          );
-        }}
-      </Query>
     </ApolloProvider>
   );
 }
