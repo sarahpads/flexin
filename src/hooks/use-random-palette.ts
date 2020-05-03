94e35a8f897ebaa2ef0a9a33d84b6113603c6ac2
Sarah <sarahpads@gmail.com>
2020-05-02 23:59:09 -0300

    missing file for useRandomPalette

---
+++ src/hooks/use-random-palette.ts
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+import theme, { Palette } from "../theme";
+
+let currentPalette: Palette;
+
+export default function useRandomPalette() {
+  const [palette, setPalette] = useState({} as Palette);
+
+  const palettes = [
+    theme.colors.palettePurple,
+    theme.colors.paletteBlue,
+    theme.colors.paletteRed,
+    theme.colors.paletteYellow
+  ];
+
+  useEffect(() => {
+    const available = palettes.filter((p) => p !== currentPalette);
+    const index: number = Math.floor(Math.random() * available.length);
+
+    setPalette(palettes[index]);
+  }, [])
+
+  return palette;
+}
