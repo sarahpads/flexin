94201d104ec39569c4247f846c563b0538fab78d
Sarah <sarahpads@gmail.com>
2020-05-02 23:22:58 -0300

    animate in leaderboard

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -15,12 +15,17 @@ const defaultHeight = 155;
 const Leaderboard: React.FC<LeaderboardProps> = ({ responses = [] }) => {
   const elRef = useRef() as React.MutableRefObject<HTMLInputElement>;
   const [elHeight, setElHeight] = useState();
-  const [props, set]: any = useSpring(() => ({ height: defaultHeight }))
-  // order by most achieved
+  // const [props, set]: any = useSpring(() => ({ height: defaultHeight }))
+  const [props, set]: any = useSpring(() => ({ height: 0 }))
+  // TODO: order by most achieved
+
+  useEffect(() => {
+    // Give state animations enough time to finish before animating in
+    setTimeout(() => set({ height: defaultHeight, config: config.wobbly }), 900);
+  }, [])
 
   useEffect(() => {
     setElHeight(elRef.current?.offsetHeight);
-    // TOOD: make this conditional on number of responses
   }, [elRef, responses]);
 
   const bind = useDrag(({
@@ -31,22 +36,20 @@ const Leaderboard: React.FC<LeaderboardProps> = ({ responses = [] }) => {
       height: clamp(memo[0] + my * -1, defaultHeight, elHeight),
       config: config.wobbly
     });
+
     return memo;
-  }, {
-    axis: "y"
-  })
+  }, { axis: "y" });
 
-  const style = {
-  }
-
-  return <S.AnimatedLeaderboard {...bind()} style={props}>
-    <S.Leaderboard ref={elRef}>
-      <S.Title>Leaderboard</S.Title>
-      {responses.map((response: any, index: number) => {
-        return <Standing key={index} response={response} rank={index}></Standing>
-      })}
-    </S.Leaderboard>
-  </S.AnimatedLeaderboard>
+  return (
+    <S.AnimatedLeaderboard {...bind()} style={props}>
+      <S.Leaderboard ref={elRef}>
+        <S.Title>Leaderboard</S.Title>
+        {responses.map((response: any, index: number) => {
+          return <Standing key={index} response={response} rank={index}></Standing>
+        })}
+      </S.Leaderboard>
+    </S.AnimatedLeaderboard>
+  )
 }
 
 export default Leaderboard;
